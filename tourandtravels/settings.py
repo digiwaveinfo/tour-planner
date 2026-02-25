52524b1d06ae0f50a064ef305d72e171739c291d
INDRAJITSINH1101 <indrajitsinh234@gmail.com>
2026-02-25 18:25:39 +0530

    feat: Add audit logging functionality with serializers, views, and URLs

--- tourandtravels/settings.py
+++ tourandtravels/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    'django_filters',
     'rest_framework_simplejwt.token_blacklist',
 
     # apps
