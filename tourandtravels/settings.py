866f347c046ec3c222e6298c2db928efdedf1f00
bhavesh <brprajapati8916@gmail.com>
2026-02-27 11:07:13 +0530

    feat: Initialize Django project settings with core configurations, installed apps, and REST framework setup.

--- tourandtravels/settings.py
+++ tourandtravels/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +28,7 @@ SECRET_KEY = 'django-insecure-=mgo7evu5pqtkc%b2!-ql@ym9fr*zi6p7c0_s#n4km9!v1s@ey
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
