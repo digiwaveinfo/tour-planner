91913d46021f60ff407ec63084cfe42ce2175375
INDRAJITSINH1101 <indrajitsinh234@gmail.com>
2026-03-02 14:41:17 +0530

    Refactor UserViewSet to define custom queryset for user filtering

--- apps/account/views.py
+++ apps/account/views.py
@@ -7,9 +7,14 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 class UserViewSet(ModelViewSet):
-    queryset = User.objects.all()
     serializer_class = UserSerializer
-    queryset = User.objects.all().order_by("-created_at")
+    permission_classes = [IsAuthenticated]
+    def get_queryset(self):
+        user = self.request.user
+        queryset = User.objects.filter(deleted_at__isnull=True,is_active=True).order_by("-created_at")
+        queryset = queryset.filter(is_superuser=False)
+        queryset = queryset.exclude(id=user.id)
+        return queryset
 
     def get_serializer_class(self):
         if self.action in ["list", "retrieve"]:
